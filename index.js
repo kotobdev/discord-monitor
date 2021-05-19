72598cda3c50543aa694f59ee3cea01f314b5f14
KOTOB <59124164+kotobdev@users.noreply.github.com>
2021-05-19 14:30:21 -0700

    formatting :)

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ const Discord = require("discord.js");
 const { Client, MessageEmbed, MessageAttachment } = require('discord.js');
 const client = new Discord.Client();
 
-
 const channelID = "CHANNELIDHERE"
 const token = "BOTTOKENHERE"
 
-
 client.on("message", function(message) { 
-    if(message.channel.id.toString() === channelID)
+  if(message.channel.id.toString() === channelID)
   {
-    console.log(message.author.username.toString() + " says: " + message.content.toString())
+  console.log(message.author.username.toString() + " says: " + message.content.toString())
   }
-
 });
 
 client.login(token);
