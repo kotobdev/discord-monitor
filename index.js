84b2337c09fde7abf91d8b6e871b6cb4e97b5b38
KOTOB <59124164+kotobdev@users.noreply.github.com>
2021-05-19 12:59:06 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const { Client, MessageEmbed, MessageAttachment } = require('discord.js');
+const client = new Discord.Client();
+
+
+const channelID = "CHANNELIDHERE"
+const token = "BOTTOKENHERE"
+
+
+client.on("message", function(message) { 
+    if(message.channel.id.toString() === channelID)
+  {
+    console.log(message.author.username.toString() + " says: " + message.content.toString())
+  }
+
+});
+
+client.login(token);
